a8f5f2ba66ecb4bfd21ad849211a345c487176c1
lixindou <lixindou@outlook.com>
2018-04-11 10:13:41 +0800

    Update VGG16.py

--- VGG16.py
+++ VGG16.py
@@ -43,11 +43,10 @@ if __name__ == "__main__":
     im[:,:,0] -= 103.939
     im[:,:,1] -= 116.779
     im[:,:,2] -= 123.68
-#    im = im.transpose((2,0,1))
     im = np.expand_dims(im, axis=0)
 
     # Test pretrained model
-    model = VGG_16('I:/VGG/VGG_kaggle/weights/vgg16_weights_tf_dim_ordering_tf_kernels.h5')
+    model = VGG_16('I:/VGG/VGG_kaggle/weights/vgg16_weights_tf_dim_ordering_tf_kernels.h5')#网络参数路径
     sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(optimizer=sgd, loss='categorical_crossentropy')
     out = model.predict(im)
